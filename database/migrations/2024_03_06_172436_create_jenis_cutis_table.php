66b455260988b13190296391da2466ae3d904ad6
alindrapu <alindrapu@gmail.com>
2024-03-06 17:37:21 +0700

    [update] done migration jenis cuti, seeder jenis cuti

--- database/migrations/2024_03_06_172436_create_jenis_cutis_table.php
+++ database/migrations/2024_03_06_172436_create_jenis_cutis_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('jenis_cutis', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->integer('kd_jenis_cuti')->primary();
+          $table->string('nm_jenis_cuti');
+          $table->timestamps();
         });
     }
 
--- database/seeders/JenisCuti.php
+++ database/seeders/JenisCuti.php
@@ -2,16 +2,41 @@
 
 namespace Database\Seeders;
 
+use App\Models\JenisCuti as ModelJenisCuti;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class JenisCuti extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    ModelJenisCuti::create([
+      "kd_jenis_cuti" => 1,
+      "nm_jenis_cuti" => "Cuti"
+    ]);
+
+    ModelJenisCuti::create([
+      "kd_jenis_cuti" => 2,
+      "nm_jenis_cuti" => "Cuti Tidak Dibayar"
+    ]);
+
+    ModelJenisCuti::create([
+      "kd_jenis_cuti" => 3,
+      "nm_jenis_cuti" => "Cuti Sakit"
+    ]);
+
+    ModelJenisCuti::create([
+      "kd_jenis_cuti" => 4,
+      "nm_jenis_cuti" => "Cuti Melahirkan"
+    ]);
+
+    ModelJenisCuti::create([
+      "kd_jenis_cuti" => 5,
+      "nm_jenis_cuti" => "Izin"
+    ]);
+
+  }
 }
