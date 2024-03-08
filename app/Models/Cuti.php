8a06d6d467e3462d3d13afd000f40f3509b8d230
alindrapu <alindrapu@gmail.com>
2024-03-08 17:44:11 +0700

    [update] cuti and jenis cuti relation on models, [remove] cutis_table migration

--- app/Models/Cuti.php
+++ app/Models/Cuti.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cuti extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $table = "cutis";
+
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'user_id', 'id');
+  }
 }
