e76add6c933b519df1f3a035bb5a21b57083c002
alindrapu <alindrapu@gmail.com>
2024-03-12 02:08:59 +0700

    [update] getJenisCuti controller

--- app/Http/Controllers/CutiController.php
+++ app/Http/Controllers/CutiController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JenisCuti;
 use Illuminate\Http\Request;
 
 class CutiController extends Controller
 {
-    //
+    public function getJenisCuti() {
+      $listCuti = JenisCuti::select('nm_jenis_cuti', 'kd_jenis_cuti')->get();
+      $response = ['status' => 'sukses', 'value' => $listCuti];
+
+      return response()->json($response, 200);
+    }
 }
